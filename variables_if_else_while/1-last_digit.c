cf2368c4b96e4b62504997dca93789c6d4439b18
Maxel6 <6006@holbertonstudents.com>
2023-02-20 03:34:16 -0800

    task 1with newline

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -17,14 +17,14 @@ int main(void)
 	n_modulo = n % 10;
 	if (n_modulo > 5)
 	{
-		printf("Last digit of %d is %d and is greater than 5", n, n_modulo);
+		printf("Last digit of %d is %d and is greater than 5\n", n, n_modulo);
 	}
 	if (n_modulo == 0)
-		printf("Last digit of %d is %d and is 0", n, n_modulo);
+		printf("Last digit of %d is %d and is 0\n", n, n_modulo);
 	else 
 	{
 		if (n_modulo < 6)
-			printf("Last digit of %d is %d and is less than 6 and not 0", n, n_modulo);
+			printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n_modulo);
 	}
 	return (0);
 }
