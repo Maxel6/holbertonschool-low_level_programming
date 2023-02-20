de82624f08eeed98aecd58526eea90df5955221b
Maxel6 <6006@holbertonstudents.com>
2023-02-20 04:21:57 -0800

    print numbers between 0 and 9

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/* more headers goes there */
+/**
+ * main - Entry point
+ * prints all single digit numbers of base 10 starting from 0
+ * followed by a newline
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int digit;
+
+	for (digit = 0; digit < 10; digit++)
+	{
+		printf("%d", digit);
+	}
+	putchar('\n');
+	return (0);
+}
