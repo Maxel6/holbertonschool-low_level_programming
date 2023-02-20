373995ab83428b51b5f28dc99a65c98c33826fa4
Maxel6 <6006@holbertonstudents.com>
2023-02-20 05:46:08 -0800

    number 0 to 9 space and , separated

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/* more headers goes there */
+/**
+ * main - Entry point
+ * prints all single digit numbers of base 10 starting from 0
+ * followed by a , and a space
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int digit;
+
+	for (digit = 0; digit < 10; digit++)
+	{
+		putchar(digit + '0');
+		if (digit != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	return (0);
+}
