c95020d734affb690807faa7f2c2a4d1aaf08b28
Maxel6 <6006@holbertonstudents.com>
2023-02-21 05:14:12 -0800

    isalpha clone

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *_isalpha - test if the c is a letter
+ *@c: is a int
+ *Return: 1 if c is a letter , 0 else
+ */
+int _isalpha(int c)
+{
+        if (c > 'a' && c < 'z' || c > 'A' && c < 'Z')
+                c = 1;
+        else
+                c = 0;
+        return (c);
+}
+
