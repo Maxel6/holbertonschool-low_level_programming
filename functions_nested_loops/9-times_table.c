bbf63a7b5ffd13ba875f4950f855c1db1ce1f896
Maxel6 <6006@holbertonstudents.com>
2023-02-21 13:46:19 -0800

    miss 9 table and too many space un c2

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -1,26 +1,35 @@
 #include "main.h"
-void times_tables(void)
+void times_table(void)
 {
-	unsigned int x = 0;
-	unsigned int res;
+	int x = 0;
 	int y = 0;
+	int res;
 	for(y = 0; y < 10; y++)
 	{
 		for(x = 0; x < 10; x++)
 		{
 			res = x*y;
-			if (res < 10)
-				_putchar(res + '0');
-			else 
-				_putchar(res / 10 + '0');
-			putchar(res % 10 + '0');
-			if (x==9)
-				_putchar('\n');
+			if (x!=9)
+			{
+				if (res < 10)
+				{
+					_putchar(res + '0');
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+				}
+				else
+				{	
+					_putchar(res / 10 + '0');
+					_putchar(res % 10 + '0');
+					_putchar(',');
+					_putchar(' ');
+				}
+
+			}	
 			else
-				_putchar(',');
-				_putchar(' ');
+				_putchar('\n');
+
 		}
 	}
 }
-
-
