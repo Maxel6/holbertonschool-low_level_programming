86ad158b409bc9fcfa60e558a76cbd69e84198d1
Maxel6 <6006@holbertonstudents.com>
2023-02-21 05:33:10 -0800

    function give the signe of n

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_sign - display the sign of n
+ * @n: is the int we test
+ * Return: 1 if >0, 0 if ==0 and -1 if <0
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		c = 1;
+	}
+	if (n < 0)
+	{
+		_putchar('-');
+		c = -1; 
+	}
+	if (n == 0)
+		_putchar('0');
+}
