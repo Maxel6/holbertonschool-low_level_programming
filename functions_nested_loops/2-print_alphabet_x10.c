c9084f5309dd876c6d69606871bb3d83980e94b6
Maxel6 <6006@holbertonstudents.com>
2023-02-21 02:38:26 -0800

    task 2

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_alphabet_x10 - print the alphabet 10 times
+ */
+void print_alphabet_x10(void);
+{
+	char i;
+	int a;
+
+	for (a = 0; a < 11; a++)
+	{
+		for (i = 'a'; i <= 'z'; i++)
+		{
+			_putchar(i);
+		}
+		_putchar('\n');
+	}
+}
+i
