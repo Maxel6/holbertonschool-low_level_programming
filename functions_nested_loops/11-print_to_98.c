75b8deec5edfda801af1d42bd650957232a9355c
Maxel6 <6006@holbertonstudents.com>
2023-02-21 14:48:45 -0800

    print to 98

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,44 @@
+#include "main.h"
+void print_to_98(int n)
+{
+	int i;
+	if (n == 98)
+	{
+		_putchar(n / 10 + '0');
+		_putchar(n % 10 + '0');
+	}
+	if (n < 98)
+		for (i = n; i <= 98; i++)
+		{
+			if (i < 10)
+			{
+				_putchar(i + '0');
+				_putchar(',');
+				_putchar(' ');
+			}
+			if (i > 9 )
+			{
+				_putchar(i / 10 + '0');
+				_putchar(i % 10 + '0');
+				_putchar(',');
+				_putchar(' ');
+			}	
+		}
+	if (n < 98)
+		for (i = n; i >= 98; i++)
+		{
+			if (i < 10)
+			{
+				_putchar(i + '0');
+				_putchar(',');
+				_putchar(' ');
+			}
+			if (i > 9 )
+			{
+				_putchar(i / 10 + '0');
+				_putchar(i % 10 + '0');
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+}
