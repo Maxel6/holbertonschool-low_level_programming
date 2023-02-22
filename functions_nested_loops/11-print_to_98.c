e3629cd1c38be3e319cba61f87fa5e1a7f0ce613
Maxel6 <6006@holbertonstudents.com>
2023-02-22 03:31:13 -0800

    using printf

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -1,63 +1,25 @@
 #include "main.h"
+#include <stdio.h>
 void print_to_98(int n)
 {
 	int i;
-	int sec_d = i / 10 + '0';
-	int last_d = i % 10 + '0';
-	int neg
 
 	if (n == 98)
-	{
-		_putchar(i / 10 + '0');
-		_putchar(i % 10 + '0');
-		_putchar('\n');
-	}
+		printf("%d", n);
 	if (n < 98)
 	{
 		for (i = n; i <= 98; i++)
 		{
-			if (i >= 0 && i< 10)
-			{
-				_putchar(i + '0');
-				_putchar(',');
-				_putchar(' ');
-			}
-			if (i > 9 && < 100)
-			{
-				_putchar(i / 10 + '0');
-				_putchar(i % 10 + '0');
-				if (i != 98)
-				{
-					_putchar(',');
-					_putchar(' ');
-				}
-				else 
-					_putchar('\n');
-			}
+			printf("%d, ", n);
 		}
+		printf("98");
 	}
 	if (n > 98)
 	{
-		for (i = n; i >= 98; i++)
+		for (i = n; i >= 98; i--)
 		{
-			if (i < 10)
-			{
-				_putchar(i + '0');
-				_putchar(',');
-				_putchar(' ');
-			}
-			if (i > 9 )
-			{
-				_putchar(i / 10 + '0');
-				_putchar(i % 10 + '0');
-				if (i != 98)
-				{
-					_putchar(',');
-					_putchar(' ');
-				}
-				else
-					_putchar('\n');
-			}
+			printf("%d, ", i);
 		}
+		printf("98");
 	}
 }
