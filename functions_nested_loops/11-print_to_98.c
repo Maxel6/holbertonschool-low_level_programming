b0d7095423cca47dfaf8ea90e65ecb1b3a50ff77
Maxel6 <6006@holbertonstudents.com>
2023-02-22 01:06:33 -0800

    y=9 & spaces fix

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -2,14 +2,43 @@
 void print_to_98(int n)
 {
 	int i;
+	int sec_d = i / 10 + '0';
+	int last_d = i % 10 + '0';
+	int neg
+
 	if (n == 98)
 	{
-		_putchar(n / 10 + '0');
-		_putchar(n % 10 + '0');
+		_putchar(i / 10 + '0');
+		_putchar(i % 10 + '0');
 		_putchar('\n');
 	}
 	if (n < 98)
+	{
 		for (i = n; i <= 98; i++)
+		{
+			if (i >= 0 && i< 10)
+			{
+				_putchar(i + '0');
+				_putchar(',');
+				_putchar(' ');
+			}
+			if (i > 9 && < 100)
+			{
+				_putchar(i / 10 + '0');
+				_putchar(i % 10 + '0');
+				if (i != 98)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+				else 
+					_putchar('\n');
+			}
+		}
+	}
+	if (n > 98)
+	{
+		for (i = n; i >= 98; i++)
 		{
 			if (i < 10)
 			{
@@ -23,33 +52,12 @@ void print_to_98(int n)
 				_putchar(i % 10 + '0');
 				if (i != 98)
 				{
-				_putchar(',');
-				_putchar(' ');
+					_putchar(',');
+					_putchar(' ');
 				}
-				else 
-				_putchar('\n');
-			}	
-		}
-	if (n > 98)
-		for (i = n; i >= 98; i++)
-		{
-			if (i < 10)
-			{
-				_putchar(i + '0');
-				_putchar(',');
-				_putchar(' ');
-			}
-			if (i > 9 )
-			{
-				_putchar(i / 10 + '0');
-				_putchar(i % 10 + '0');
-				 if (i != 98)
-                                {
-                                _putchar(',');
-                                _putchar(' ');
-                                }
-                                else
-                                _putchar('\n');
+				else
+					_putchar('\n');
 			}
 		}
+	}
 }
--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -1,3 +1,4 @@
+#include "main.h"
 void times_table(void)
 {
 	int x = 0;
@@ -8,28 +9,29 @@ void times_table(void)
 		for(x = 0; x < 10; x++)
 		{
 			res = x*y;
-			if (x!=9)
+			if (y != 0)
 			{
 				if (res < 10)
 				{
-					_putchar(res + '0');
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
+					_putchar(res + '0');
 				}
-				else
+				if (res >= 10)
+
 				{
+					_putchar(',');
+					_putchar(' ');
 					_putchar(res / 10 + '0');
 					_putchar(res % 10 + '0');
-					_putchar(',');
-					_putchar(' ');
 				}
 
 			}
 			else
-				_putchar('\n');
-
+				_putchar('0');
 		}
+		_putchar('\n');
 	}
 }
 
