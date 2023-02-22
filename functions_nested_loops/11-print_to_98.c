6b0862ee7e0a7409adf673ced6829ed57097da0d
Maxel6 <6006@holbertonstudents.com>
2023-02-22 05:37:13 -0800

    more doc

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -1,7 +1,8 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * print_to-98 -  prints all natural numbers from n to 98.
+ * print_to_98 -  prints all natural numbers from n to 98.
+ * @n: the number we start
  */
 void print_to_98(int n)
 {
