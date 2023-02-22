30a891c0645650ab5c8755b008bdf5b4dfd6c476
Maxel6 <6006@holbertonstudents.com>
2023-02-22 03:32:38 -0800

    adding \n

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -5,14 +5,14 @@ void print_to_98(int n)
 	int i;
 
 	if (n == 98)
-		printf("%d", n);
+		printf("%d\n", n);
 	if (n < 98)
 	{
 		for (i = n; i <= 98; i++)
 		{
 			printf("%d, ", n);
 		}
-		printf("98");
+		printf("98\n");
 	}
 	if (n > 98)
 	{
@@ -20,6 +20,6 @@ void print_to_98(int n)
 		{
 			printf("%d, ", i);
 		}
-		printf("98");
+		printf("98\n");
 	}
 }
