f5ff93a9367e30db43003df3fc962e29daffc6d6
Maxel6 <6006@holbertonstudents.com>
2023-02-21 21:51:21 -0800

    added \n

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -6,6 +6,7 @@ void print_to_98(int n)
 	{
 		_putchar(n / 10 + '0');
 		_putchar(n % 10 + '0');
+		_putchar('\n');
 	}
 	if (n < 98)
 		for (i = n; i <= 98; i++)
@@ -29,7 +30,7 @@ void print_to_98(int n)
 				_putchar('\n');
 			}	
 		}
-	if (n < 98)
+	if (n > 98)
 		for (i = n; i >= 98; i++)
 		{
 			if (i < 10)
