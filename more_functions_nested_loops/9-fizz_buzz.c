de74b2d4fb7bbd05b553cc3b56bc7cd3c3890415
Maxel6 <6006@holbertonstudents.com>
2023-02-23 11:16:14 -0800

    fizzbuzz

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 100; i++)
+	{
+
+		if (i % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		if (i % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		if ((i % 3 != 0) && (i % 5 != 0))
+		{
+			printf("%d", i);
+		}
+		putchar(' ');
+	}
+	return (0);
+
+}
