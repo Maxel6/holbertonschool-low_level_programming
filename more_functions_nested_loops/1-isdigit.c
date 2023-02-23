b148c60dbb3e58b34f4c8213463725cddbc1bcc6
Maxel6 <6006@holbertonstudents.com>
2023-02-23 01:05:31 -0800

    itsdigit

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,13 @@
+/**
+ *_isupper - checks for a digit.
+ *@c: digit we need to test
+ *Return: 1 if is a digit 0 else
+ */
+#include "main.h"
+int _isupper(int c)
+{
+	if (c >= '0' && c <= '9')
+		c = 1;
+	else
+		c = 0;
+	return (c);
