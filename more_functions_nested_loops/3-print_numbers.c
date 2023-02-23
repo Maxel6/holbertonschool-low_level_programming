eaa407cd6606d0124c37616c86a650d43350358a
Maxel6 <6006@holbertonstudents.com>
2023-02-23 01:42:01 -0800

    corrected betty

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,10 +1,14 @@
 #include "main.h"
+/**
+ * print_numbers - print the numbers from 0 to 9
+ */
 void print_numbers(void)
 {
 	int idx;
+
 	for (idx = 0; idx < 10; idx++)
 	{
-		_putchar(idx);
+		_putchar(idx +  '0');
 	}
 	_putchar('\n');
 }
