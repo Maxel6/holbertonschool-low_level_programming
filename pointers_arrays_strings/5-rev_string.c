c1b7f75ef02fbda1ad649f074abbd40a22f3d564
Maxel6 <6006@holbertonstudents.com>
2023-02-27 05:15:14 -0800

    replace _strlen by its def

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -1,14 +1,20 @@
 #include "main.h"
-#include <stdio.h>
+
 void rev_string(char *s)
 {
-	int len = _strlen(s);
+	int len = 0;
 	int i;
-	char temp;
-	for (i = 0; i < len/2; i++) {
-		temp = s[i];
+	char tmp;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+	for (i = 0; i < len/2; i++) 
+	{
+		tmp = s[i];
 		s[i] = s[len-i-1];
-		s[len-i-1] = temp;
+		s[len-i-1] = tmp;
 	}
 
 }
