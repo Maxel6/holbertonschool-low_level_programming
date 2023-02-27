f140f405d2277a847aec415628385983a517750a
Maxel6 <6006@holbertonstudents.com>
2023-02-27 05:17:34 -0800

    corrected betty

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -1,5 +1,8 @@
 #include "main.h"
-
+/**
+ *rev_string - reverse a string
+ *@s: is a char of a string
+ */
 void rev_string(char *s)
 {
 	int len = 0;
@@ -10,11 +13,10 @@ void rev_string(char *s)
 	{
 		len++;
 	}
-	for (i = 0; i < len/2; i++) 
+	for (i = 0; i < len / 2; i++)
 	{
 		tmp = s[i];
-		s[i] = s[len-i-1];
-		s[len-i-1] = tmp;
+		s[i] = s[len - i - 1];
+		s[len - i - 1] = tmp;
 	}
-
 }
