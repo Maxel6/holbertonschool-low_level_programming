69848e3d04382129867b3ef16d97bebd8f9078d2
Maxel6 <6006@holbertonstudents.com>
2023-02-27 05:23:40 -0800

    put only pair char

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *_puts - print only pair
+ *@str: is a char of the string we want print
+ */
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (str[i] % 2 == 0)
+			_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
