d737d62a4a5d242a04201310e63f2e4f4fc043f6
Maxel6 <6006@holbertonstudents.com>
2023-02-27 09:04:13 -0800

    fix of cdt

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -2,30 +2,16 @@
 
 void puts_half(char *str)
 {
-	int i = 0;
 	int len = 0;
-
+	int i = 0;
 	while (str[len] != '\0')
 	{
 		len++;
 	}
-	while (str[i] <= len)
+	for (;i < len; i++)
 	{
-		if (len % 2 == 1)
-		{
-			if (i > ((len - 1) / 2))
-			{				
-				_putchar(str[i]);
-			}	
-		}
-		if (len % 2 == 0)
-		{
-			if (i > (len / 2))
-			{
-				_putchar(str[i]);
-			}
-		}
-		i++;
+		if (i > ((len - 1) / 2))
+		_putchar(str[i]);
 	}
 	_putchar('\n');
 }
