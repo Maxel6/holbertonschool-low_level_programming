8ffd961931cf54b4d88c47f5950fec015afff8fd
Maxel6 <6006@holbertonstudents.com>
2023-02-27 09:07:01 -0800

    corrected betty

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -1,17 +1,23 @@
 #include "main.h"
-
+/**
+ *puts_half - print the seconf half of a string
+ *@str: a char of the string
+ */
 void puts_half(char *str)
 {
 	int len = 0;
 	int i = 0;
+
 	while (str[len] != '\0')
 	{
 		len++;
 	}
-	for (;i < len; i++)
+	for (; i < len; i++)
 	{
 		if (i > ((len - 1) / 2))
+		{
 		_putchar(str[i]);
+		}
 	}
 	_putchar('\n');
 }
