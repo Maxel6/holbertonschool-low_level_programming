fe7aa31a87393525109d9b4b835db518862b5e04
Maxel6 <6006@holbertonstudents.com>
2023-02-28 12:57:58 -0800

    fix first char

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -26,7 +26,7 @@ char *cap_string(char *str)
 			if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
 				str[i + 1] -= 32;
 		}
-		if (str[i] == '}' || str[i] == 1)
+		if (str[i] == '}' || i == 0)
 		{
 			if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
 				str[i + 1] -= 32;
