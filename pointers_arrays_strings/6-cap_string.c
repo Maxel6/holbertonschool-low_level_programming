21e8e6d2aada1f9926e94647a793e2c5f3fead3c
Maxel6 <6006@holbertonstudents.com>
2023-02-28 13:03:25 -0800

    corrected betty

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -32,10 +32,10 @@ char *cap_string(char *str)
 				str[i + 1] -= 32;
 		}
 		if (i == 0)
-                  {
-                          if (str[i] >= 'a' && str[i] <= 'z')
-                                  str[i] -= 32;
-                  }
+		{
+			if (str[i] >= 'a' && str[i] <= 'z')
+				str[i] -= 32;
+		}
 		i++;
 	}
 	return (str);
