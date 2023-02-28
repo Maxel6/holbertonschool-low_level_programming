137a4d4acc5d6f4c55ec85df530c3fb162113bd2
Maxel6 <6006@holbertonstudents.com>
2023-02-28 01:44:45 -0800

    fix segfault

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -10,16 +10,18 @@ char *_strcat(char *dest, char *src)
 	int len = 0;
 	int i = 0;
 
-	while (dest[len] != '\n')
+	while (dest[len] != '\0')
 	{
 		len++;
 	}
-	while (src[i] != '\n')
+	while (src[i] != '\0')
 	{
+
+
 		dest[len] = src[i];
-		i++;
 		len++;
+		i++;
 	}
-	dest[len] = '\n';
+	dest[len] = '\0';
 	return (dest);
 }
