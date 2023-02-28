3bbd3183b7aa5629a956d2b6e7f2abef0df9079e
Maxel6 <6006@holbertonstudents.com>
2023-02-28 12:55:02 -0800

    fix start

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -1,26 +1,34 @@
+#include "main.h"
+/**
+ * cap_string - capitalizes all words of a string
+ * @str: point the array to modify
+ * Return: the string
+ */
+
 char *cap_string(char *str)
 {
 	int i = 0;
+
 	while (str[i] != '\0')
 	{
-		if(str[i] == ' ' || str[i] == 9 || str[i] == '\n' || str[i] == ',')
+		if (str[i] == ' ' || str[i] == 9 || str[i] == '\n' || str[i] == ',')
 		{
-			if(str[i + 1] >= 'a' && str[i + 1] <= 'z')
+			if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
 				str[i + 1] -= 32;
 		}
-		if(str[i] == ';' || str[i] == '.' || str[i] == '!' || str[i] == '?')
+		if (str[i] == ';' || str[i] == '.' || str[i] == '!' || str[i] == '?')
 		{
-			if(str[i + 1] >= 'a' && str[i + 1] <= 'z')
+			if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
 				str[i + 1] -= 32;
 		}
-		if(str[i] == '"' || str[i] == '(' || str[i] == ')' || str[i] == '{')
+		if (str[i] == '"' || str[i] == '(' || str[i] == ')' || str[i] == '{')
 		{
-			if(str[i + 1] >= 'a' && str[i + 1] <= 'z')
+			if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
 				str[i + 1] -= 32;
 		}
-		if(str[i] == '}')
+		if (str[i] == '}' || str[i] == 1)
 		{
-			if(str[i + 1] >= 'a' && str[i + 1] <= 'z')
+			if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
 				str[i + 1] -= 32;
 		}
 		i++;
