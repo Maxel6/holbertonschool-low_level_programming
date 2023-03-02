fffcf1b140ecb2be33203c325226a1abb39ca3c3
Maxel6 <6006@holbertonstudents.com>
2023-03-02 06:26:20 -0800

    added doc

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -1,14 +1,19 @@
 #include "main.h"
+/**
+ * print_chessboard -prints the chessboard.
+ * @a: a matrix
+ */
 void print_chessboard(char (*a)[8])
 {
 	int y;
 	int x = 0;
+
 	for (y = 0; y < 8; y++)
 	{
 		for (x = 0; x < 8; x++)
 		{
 			_putchar(a[y][x]);
-		}	
+		}
 		_putchar('\n');
 	}
 }
