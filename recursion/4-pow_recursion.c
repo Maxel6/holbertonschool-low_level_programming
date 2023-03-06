0cb126fdd4a0bd0bc81cf36907f30cb7f92d18ed
Maxel6 <6006@holbertonstudents.com>
2023-03-06 01:47:00 -0800

    my pow recursive function

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _pow_recursion - return the value of x raised to the power of y
+ * @x: is the number to multiplie
+ * @y: is the power of x
+ * Return: x^y
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y == 0)
+		return (1);
+	if (y > 0)
+		return (x * _pow_recursion(x, y - 1));
+	else
+		return (1 / _pow_recursion(x, -y));
+}
