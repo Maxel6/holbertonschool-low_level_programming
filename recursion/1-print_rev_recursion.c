513db8865226663c98b182468d580fdebb9a41ff
Maxel6 <6006@holbertonstudents.com>
2023-03-06 01:07:13 -0800

    prints a string in reverse

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *_print_rev_recursion - prints a string in reverse.
+ *@s: is a char of a string
+ */
+#include <stdio.h>
+
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
