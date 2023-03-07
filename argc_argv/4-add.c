645fc62f8a0a16bb9d8bc405c6e391ce791db633
Maxel6 <6006@holbertonstudents.com>
2023-03-07 01:34:38 -0800

    add

---
+++ argc_argv/4-add.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - program that multiplies two numbers.
+ * @argc: length of the argv array
+ * @argv: list all the entry
+ * Return: 0 if the execution is ok, 1 If don't not receive two arguments
+ */
+int main(int argc, char *argv[])
+{
+	int res = 0;
+	int tmp = 0;
+	int i;
+	if (argc == 0)
+	{
+		printf("0\n");
+		return (1);
+	}
+	for (i = 1; i < argc; i++)
+	{
+		tmp = atoi(argv[i]);
+		if (tmp <= 0)
+		{
+			printf("Error\n");
+			return (1);
+		}
+		else
+			res += tmp;
+	}
+	printf("%d\n", res);
+	return (0);
+
+}
