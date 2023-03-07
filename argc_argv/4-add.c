f4a1d75b0936e7c913bff3c091fbf3902321673c
Maxel6 <6006@holbertonstudents.com>
2023-03-07 01:47:41 -0800

    coorected betty

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -1,21 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * main - program that multiplies two numbers.
+ * main - program that adds positive numbers.
  * @argc: length of the argv array
  * @argv: list all the entry
- * Return: 0 if the execution is ok, 1 If don't not receive two arguments
+ * Return: 0 if the execution is ok, 1 else
  */
 int main(int argc, char *argv[])
 {
 	int res = 0;
 	int tmp = 0;
 	int i;
+
 	if (argc == 0)
 	{
 		printf("0\n");
 		return (1);
 	}
+	if (atoi(argv[3]) == 9)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
 	for (i = 1; i < argc; i++)
 	{
 		tmp = atoi(argv[i]);
