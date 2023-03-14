9c683796ed14d9e8f8fa6419578b87e2d020ad58
Axel Malka <axelmalka@outlook.fr>
2023-03-14 02:09:56 -0700

    fix null case

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -17,11 +17,17 @@ char *str_concat(char *s1, char *s2)
 
 	if (s1 == 0 && s2 == 0)
 		return (0);
+	if (s1 == 0)
+		return (s2);
+	if (s2 == 0)
+		return (s1);
 	while (s1[len1])
 		len1++;
 	while (s2[len2])
 		len2++;
 	dest = malloc(sizeof(char) * (len1 + len2) + 1);
+	if (dest = 0)
+		return (0);
 	for (i = 0; s1[i]; i++)
 		dest[i] = s1[i];
 	for (j = 0; s2[j]; i++, j++)
