cd8e212e003666a32f6ccd81baa49b4a92bde913
Axel Malka <axelmalka@outlook.fr>
2023-03-14 06:55:30 -0700

    corrected betty

--- malloc_free/4-free_grid.c
+++ malloc_free/4-free_grid.c
@@ -1,10 +1,14 @@
 #include "main.h"
 #include <stdlib.h>
-
+/**
+ *free_grind - free a grind
+ *@grind: the array to free
+ *@height: the height of the array
+ */
 void free_grid(int **grid, int height)
 {
 	int i;
-	
+
 	for (i = 0; i < height; i++)
 		free(grid[i]);
 	free(grid);
