a13becfddf2c6d8aa768df63e599b391cf92e1fc
Axel Malka <axelmalka@outlook.fr>
2023-03-14 07:19:01 -0700

    adding documentation

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -1,6 +1,10 @@
 #include "main.h"
 #include <stdlib.h>
-
+/**
+ * malloc_checked - allocate memory
+ * @b: the size to allocate
+ * Return: a memory adress
+ */
 void *malloc_checked(unsigned int b)
 {
 	void *p = 0;
