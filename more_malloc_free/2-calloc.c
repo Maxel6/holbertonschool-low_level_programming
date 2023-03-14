b8715c2721e2f8943b63b5531c0b29e0f94a2928
Axel Malka <axelmalka@outlook.fr>
2023-03-14 08:37:21 -0700

    set to 0

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -4,11 +4,13 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	int *p = 0;
-	
+	int i;	
 	if (size == 0 || nmemb == 0)
 		return (0);
 	p = malloc(nmemb * size);
 	if (p == 0)
 		return (0);
+	for (i = 0; i < size; i++)
+		p[i] = 0;
 	return (p);
 }
