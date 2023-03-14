606882db82d08c2701bcb4591732b23c3473cdc0
Axel Malka <axelmalka@outlook.fr>
2023-03-14 08:33:58 -0700

    calloc

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include "stdlib.h"
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	int *p = 0;
+	
+	if (size == 0 || nmemb == 0)
+		return (0);
+	p = malloc(nmemb * size);
+	if (p == 0)
+		return (0);
+	return (p);
+}
