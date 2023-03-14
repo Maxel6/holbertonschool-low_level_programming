f8f39f7231c548bb2cce1d60c8e8a3374be8ebe0
Axel Malka <axelmalka@outlook.fr>
2023-03-14 07:30:16 -0700

    try to push

---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdlib.h>
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *dest = 0;
+	unsigned int len1 = 0;
+	unsigned int len2 = 0;
+	unsigned int i;
+	unsigned int j;
+
+	if (s1 == 0)
+		s1 = "";
+	if (s2 == 0)
+		s2 = "";
+	while (s1[len1])
+		len1++;
+	while (s2[len2])
+		len2++;
+	if (n > len2)
+		n = len2;
+	dest = malloc(sizeof(char) * (len1 + len2) + 1);
+	if (dest == 0)
+		return (0);
+	for (i = 0; s1[i]; i++)
+		dest[i] = s1[i];
+	for (j = 0; j != n; i++, j++)
+		dest[i] = s2[j];
+	return (dest);
+}
