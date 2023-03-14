a1a45806c6bb92005ce7bb453044d4ba8cd7ecd1
Axel Malka <axelmalka@outlook.fr>
2023-03-14 11:23:25 -0700

    corrected betty

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -1,17 +1,21 @@
 #include "main.h"
 #include <stdlib.h>
-
+/**
+ * _calloc - allocates memory for an array
+ * @nmemb: number of array elements
+ * @size: number of bytes of array elements
+ */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	char *p = 0;
-	unsigned i = 0;
+	unsigned int i = 0;
 
 	if (size == 0 || nmemb == 0)
 		return (0);
 	p = malloc(nmemb * size);
 	if (p == 0)
 		return (0);
-	while(i < nmemb * size)
+	while (i < nmemb * size)
 	{
 		p[i] = 0;
 		i++;
