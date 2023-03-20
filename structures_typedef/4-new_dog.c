fe3513bfef2d4d7901a133a9ee7f30fed548be28
Axel Malka <axelmalka@outlook.fr>
2023-03-20 05:14:21 -0700

    rm pointer cpoy

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -3,42 +3,31 @@
 #include <stdlib.h>
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	char *cpname = NULL, *cpowner = NULL;
 	int namelen = 0, owlen = 0, i = 0;
 	dog_t *dog = malloc(sizeof(dog_t));
 	
 	if (dog == NULL)
-	{
-		free(dog);
 		return (NULL);
-	}
 	while (owner[owlen])
 		owlen++;
 	while (name[namelen])
 		namelen++;
-        cpname = malloc(sizeof(char) * namelen + 1);
-	cpowner = malloc(sizeof(char) * owlen + 1);
+	
 	dog->name = malloc(sizeof(char) * namelen + 1);
 	dog->owner = malloc(sizeof(char) * owlen + 1);
-	if (cpname == NULL || cpowner == NULL || dog->name == NULL || dog->owner == NULL)
-		return (NULL);
-	for (i = 0; i < owlen; i++)
-		cpowner[i] = owner[i];
-	cpowner[i] = 0;
-	for (i = 0; i < namelen; i++)
-		cpname[i] = name[i];
-	cpname[i] = 0;
-	if (dog == NULL)
+	if (dog->name == NULL || dog->owner == NULL)
 	{
-		free(dog->name);
 		free(dog->owner);
-		free(cpname);
-		free(cpowner);
+		free(dog->name);
 		free(dog);
 		return (NULL);
 	}
-	dog->name = cpname;
+	for (i = 0; i < owlen; i++)
+		dog->owner[i] = owner[i];
+	dog->owner[i] = '\0';
+	for (i = 0; i < namelen; i++)
+		dog->name[i] = name[i];
+	dog->name[i] = '\0';
 	dog->age = age;
-	dog->owner = cpowner;
 	return (dog);
 }
