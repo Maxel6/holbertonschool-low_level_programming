d3334944d090ab04be58e8fa2ca757040befd2bf
Axel Malka <axelmalka@outlook.fr>
2023-03-20 03:22:36 -0700

    free dog irst

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -8,7 +8,10 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *dog = malloc(sizeof(dog_t));
 	
 	if (dog == NULL)
+	{
+		free(dog);
 		return (NULL);
+	}
 	while (owner[owlen])
 		owlen++;
 	while (name[namelen])
