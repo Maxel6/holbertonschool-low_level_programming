dc0b266ff217111c11ae1682a3dbb860e619ae7e
Axel Malka <axelmalka@outlook.fr>
2023-03-20 06:05:25 -0700

    adding documentation

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,5 +1,9 @@
 #include "function_pointers.h"
-
+/**
+ * print_name - print a string using the function passed
+ * @name: the string to print
+ * @f: the print fonction to use
+ */
 void print_name(char *name, void (*f)(char *))
 {
 	if (name != NULL && f != NULL)
