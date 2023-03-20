18e625349d2e8b916537e5a997589f1997ee2cc3
Axel Malka <axelmalka@outlook.fr>
2023-03-20 01:50:07 -0700

    adding doc

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
-
+/**
+ * struct dog - informations of a dog
+ * @name: name of the dog : char*
+ * @age: age of the dog : float
+ * @owner: the dog owner : char*
+ *
+ * Description: contains the age, name and owner of a dog
+ */
 struct dog
 {
 	char *name;
