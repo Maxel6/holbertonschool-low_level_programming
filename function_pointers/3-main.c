2646deaf6b25b2a3d684e0c8333febc3378ac26f
Axel Malka <axelmalka@outlook.fr>
2023-03-21 01:50:15 -0700

    initial commit

---
+++ function_pointers/3-main.c
@@ -0,0 +1,2 @@
+#include "3-calc.h"
+int main(char **argc, int argc)
---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,54 @@
+#include "3-calc.h"
+/**
+ * op_add - add a and b
+ * @a: is an integer
+ * @b: is an integer
+ * Return: the sum of a and b
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - sub a and b
+ * @a: is an integer
+ * @b: is an integer
+ * Return: the difference of a and b
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - multiplie a and b
+ * @a: is an integer
+ * @b: is an integer
+ * Return: the product of a and b
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - div a and b
+ * @a: is an integer
+ * @b: is an integer
+ * Return: the result of the division of a and b
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - mod a and b
+ * @a: is an integer
+ * @b: is an integer
+ * Return: the remainder of the divisionof a and b
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
