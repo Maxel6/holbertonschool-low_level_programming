1e4c3fbf077cb80d0dcb4048248d6a2d882a7579
Axel Malka <axelmalka@outlook.fr>
2023-03-22 08:05:29 -0700

    fix null string

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -1,20 +1,23 @@
 #include "variadic_functions.h"
 /**
- * print_numbers - print all numbers in arguments
- * @separator: the char between each numbers
+ * print_strings - print all numbers in arguments
+ * @separator: the char between each strings
  * @n: number of elements
  */
 void print_strings(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i = 0;
 	va_list op;
-	char* str = 0;
+	char *str = 0;
 
 	va_start(op, n);
 	for (i = 0; i < n; i++)
 	{
 		str = va_arg(op, char*);
-		printf("%s", str);
+		if (str)
+			printf("%s", str);
+		else
+			printf("(nil)")
 		if (i != n - 1 && separator != NULL)
 			printf("%s", separator);
 	}
