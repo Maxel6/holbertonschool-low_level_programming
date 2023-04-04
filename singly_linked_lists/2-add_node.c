e3a4d47a6c28d49a9627f1afe72c7ae8dd0d4258
Axel Malka <axelmalka@outlook.fr>
2023-04-04 07:25:41 -0700

    add len in new node

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -1,15 +1,22 @@
 #include "lists.h"
+/**
+ * add_node - create a new node
+ * @head: is a single linked list of type list_t
+ * @str: the string to copy in the new node
+ * Return: the new node
+ */
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node;
 
 	new_node = malloc(sizeof(list_t) * 1);
 	if (!new_node)
-		return(NULL);
+		return (NULL);
 
 	new_node->str = strdup(str);
+	new_node->len = strlen(str);
 	new_node->next = *head;
 	*head = new_node;
-	
+
 	return (new_node);
 }
