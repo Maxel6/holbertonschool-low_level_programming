7c127f337ed57ba5a6536b4347231b57d0db4f33
Axel Malka <axelmalka@outlook.fr>
2023-04-04 11:04:43 -0700

    fix null list case

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -1,9 +1,17 @@
 #include "lists.h"
-
+/**
+ * print_dlistint - print all integers of a list
+ * @h: double linked list
+ * Return: the number of elements
+ */
 size_t print_dlistint(const dlistint_t *h)
 {
 	int count = 0;
 
+	if (h == NULL)
+	{
+		return (0);
+	}
 	while (h->next)
 	{
 		printf("%d\n", h->n);
