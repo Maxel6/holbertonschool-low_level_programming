11986ceb4b7a467572618ad284052c733b9d03cc
Axel Malka <axelmalka@outlook.fr>
2023-04-04 11:09:44 -0700

    is like the previous one but easyer

---
+++ doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * dlistint_len - count the number of elements in a linked
+ * @h: the double linked list
+ * Return: the number of elements
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	int count = 1;
+
+	while (h->next)
+	{
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
