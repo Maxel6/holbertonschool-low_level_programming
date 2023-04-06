1a297696645252159cd714c68fd397c5659698b2
Axel Malka <axelmalka@outlook.fr>
2023-04-06 08:22:23 -0700

    0x to b10

---
+++ bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int res = 0;
+	int len = 0, i = 0;
+
+	while (b[len])
+		len++;
+	for (i = len - 1; i >= 0; i--)
+	{
+		if (b[i] == '1')
+		{
+			res <<= 1;
+			res = res | 0x01;
+		}
+		else if (b[i] == '0')
+			res <<= 1;
+		else
+			return (0);
+	}
+	return (res);
+}
