a42fb75714bd7beef7cf58579009f5037b8fbae3
Axel Malka <axelmalka@outlook.fr>
2023-04-07 02:57:54 -0700

    add main.h

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -5,10 +5,10 @@ unsigned int binary_to_uint(const char *b)
 	unsigned int res = 0;
 	int len = 0, i = 0;
 
+	if (b == NULL)
+		return (0);
 	while (b[len])
 		len++;
-	if (len == 0)
-		return (0);
 	for (i = len - 1; i >= 0; i--)
 	{
 		if (b[i] == '1')
