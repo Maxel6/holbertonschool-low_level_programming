d4f3a66619073f6dc13a4a06948071bb4586dfed
Axel Malka <axelmalka@outlook.fr>
2023-04-07 00:54:38 -0700

    add if for next

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -11,13 +11,16 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	dlistint_t *new_node;
 
 	new_node = malloc(sizeof(dlistint_t) * 1);
-	if (!new_node)
+	if (!new_node || !head)
 		return (NULL);
 
 	new_node->prev = NULL;
 	new_node->n = n;
-	new_node->next = *head;
-	(*head)->prev = new_node;
+	if (head)
+	{
+		new_node->next = *head;
+		(*head)->prev = new_node;
+	}
 	*head = new_node;
 	return (*head);
 }
