fc9891505d090814c2670897910601d48b55cd14
= <=>
2023-04-09 17:25:24 +0200

    fix return

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -1,38 +1,46 @@
 #include "lists.h"
+/**
+ *insert_dnodeint_at_index - insert a new node at index
+ *@h: the dlistint
+ *@idx: the index for new node
+ *@n: the data of the new node
+ *Return: the adress of the new node
+ */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
     dlistint_t *new = malloc(sizeof(dlistint_t) * 1);
-    dlistint_t *tmp = malloc(sizeof(dlistint_t) * 1);
-    dlistint_t *tmp2 = malloc(sizeof(dlistint_t) * 1);
-    unsigned int i = 0;
+    dlistint_t *tmp = *h;
+    dlistint_t *tmp2 = NULL;
     
-    if (!h || !*h || !new || !tmp || !tmp2)
+    if (!new)
+    {
+        free(new);
+        printf("!new");
         return (NULL);
-    new->n = n;
-    tmp = *h;
+    }
 
-         while (idx != 1)
-         {
-            tmp = tmp->next;
-            i--;
-         }
-         if (!tmp->prev)
-         {
-            new->prev = NULL;
-            tmp->next = new;
-         }
-         if (!tmp->next)
-         {
-            tmp->next = new;
-            new->prev = tmp;
-         }
-         else
-         {
-         tmp2 = tmp->next;
-         tmp->next = new;
-         tmp2->prev = new;
-         new->next = tmp2;
-         new->prev = tmp;
-         } 
+    if (*h == NULL && idx)
+        return NULL;
+
+    if (!idx)
+        return (add_dnodeint(h, n));
+
+   
+    for (; idx != 1; idx--)
+    {
+        if(!tmp->next)
+            return (NULL);
+        tmp = tmp->next;
+    }
+    if (!tmp->next)
+        return (add_dnodeint_end(h, n));
+    
+    tmp2 = tmp->next;
+    tmp->next = new;
+    tmp2->prev = new;
+    new->n = n;
+    new->prev = tmp;
+    new->next = tmp2;
+
     return (new);
 }
