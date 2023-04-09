312623d47aab64110341872e6b7f4171b2ddc207
= <=>
2023-04-09 17:53:15 +0200

    get a bit at index

---
+++ bit_manipulation/2-get_bit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * get_bit - Return the value of a bit at a given index
+ * @n: Number to check
+ * @index: Index to check
+ * Return: The value of the bit at index on SUCCESS, -1 else
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (index >= (sizeof(unsigned long) * 8))
+		return (-1);
+
+	return ((n >> index) | 1);
+}
