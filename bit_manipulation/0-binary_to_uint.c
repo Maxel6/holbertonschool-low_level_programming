0c7b33d08182ed63b13033082d3ffd542046c15c
= <=>
2023-04-09 17:36:58 +0200

    fix start w/ 1 case

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -1,28 +1,27 @@
 #include "main.h"
+/**
+ * binary_to_uint - convert a binary number to an unsigned int
+ * 
+ * @b: the string cointains the binary
+ * Return: unsigned int result
+ */
 
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int res = 0;
-	int len = 0, i = 0;
+
+	int res = 0;
 
 	if (b == NULL)
 		return (0);
-	while (b[len])
-		len++;
-	for (i = len - 1; i >= 0; i--)
+	
+	while (*b)
 	{
-		if (b[i] == '1')
-		{
-			res <<= 1;
-			res = res | 1;
-		}
-		else if (b[i] == '0')
-		{
-			res <<= 1;
-			res = res | 0;
-		}
-		else
+		if (*b != '0' && *b != '1')
 			return (0);
+
+		res = (res << 1) + *b - '0';
+		b++;
 	}
+
 	return (res);
 }
