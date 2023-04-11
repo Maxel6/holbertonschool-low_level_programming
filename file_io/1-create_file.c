e7bd771ba1d74f3493a6a8a37787ae3c8d19e7f7
Axel Malka <axelmalka@outlook.fr>
2023-04-11 03:17:24 -0700

    betty corrected

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -1,18 +1,24 @@
 #include "main.h"
-
+/**
+ * create_file - create or open a file to write it
+ * @filename: the name of the file to open or create
+ * @text_content: the text to write in the file
+ * Return: 1 on success, -1 on failure
+ */
 int create_file(const char *filename, char *text_content)
 {
-    int file;
-    if (!filename)
-        return (-1);
-    if (!text_content)
-        text_content = "";
+	int file;
 
-    file = open(filename, O_CREAT | O_WRONLY | O_TRUNC);
-    if (file == -1)
-        return (-1);
+	if (!filename)
+		return (-1);
+	if (!text_content)
+		text_content = "";
 
-    write(file, text_content, strlen(text_content));
-    close(file);
-    return (1);
+	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 6000);
+	if (file == -1)
+		return (-1);
+
+	write(file, text_content, strlen(text_content));
+	close(file);
+	return (1);
 }
