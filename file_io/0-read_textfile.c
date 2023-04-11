33d42116ca447f6ad5ecbcd9901287784226ce01
= <=>
2023-04-11 11:37:10 +0200

    betty corrected

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -1,42 +1,37 @@
 #include "main.h"
+/**
+ * read_textfile - read a text file and prints it to the POSIX standard output
+ * @filename: name of the input file
+ * @letters: number of chars to print
+ * Return: the number of printed char
+ */
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int file, idx = 0;
-	char *str;
-	
+    int file, idx = 0;
+    char *str;
 
-	if (!filename)
+    if (!filename)
     {
         return (0);
     }
-		
-
-	file = open(filename, O_RDONLY);
-
-	if (file == -1)
+    file = open(filename, O_RDONLY);
+    if (file == -1)
     {
         return (0);
     }
-		
-
-	str = malloc(sizeof(char) * letters + 1);
-
+    str = malloc(sizeof(char) * letters + 1);
     if (!str)
     {
-         return (0);
+        return (0);
     }
-       
-
-	read(file, str, letters);
-	str[letters] = '\0';
-
-	while (str[idx])
+    read(file, str, letters);
+    str[letters] = '\0';
+    while (str[idx])
     {
-		write(STDOUT_FILENO, &str[idx], 1);
+        write(STDOUT_FILENO, &str[idx], 1);
         idx++;
     }
-
-	close(file);
-	return (strlen(str));
+    close(file);
+    return (strlen(str));
 }
