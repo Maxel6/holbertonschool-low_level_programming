3ccdb16c91fd951f7234485f3f9f89e54184073e
Axel Malka <axelmalka@outlook.fr>
2023-04-11 03:35:20 -0700

    betty corrected

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -14,7 +14,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (!text_content)
 		return (1);
 
-	file = open(filename,O_CREAT | O_WRONLY | O_APPEND);
+	file = open(filename, O_CREAT | O_WRONLY | O_APPEND);
 	if (file == -1)
 		return (-1);
 
