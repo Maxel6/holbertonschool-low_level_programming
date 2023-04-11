46d26fea57c26266362c1fbdaf7f7b79054d37af
= <=>
2023-04-11 11:28:02 +0200

    read input file

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int file, idx = 0;
+	char *str;
+	
+
+	if (!filename)
+		return (0);
+
+	file = open(filename, O_RDONLY);
+
+	if (file == -1)
+		return (0);
+
+	str = malloc(sizeof(char) * letters + 1);
+    if (!str)
+        return (0);
+
+	read(file, str, letters);
+	str[letters] = '\0';
+
+	while (str[idx])
+    {
+		write(STDOUT_FILENO, &str[idx], 1);
+        idx++;
+    }
+
+	close(file);
+	return (strlen(str));
+}
