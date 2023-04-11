b285c975d5a2099ad10ec5581ef168e08f6fb47e
= <=>
2023-04-11 12:18:41 +0200

    fix premition

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -14,7 +14,7 @@ int create_file(const char *filename, char *text_content)
 	if (!text_content)
 		text_content = "";
 
-	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 6000);
+	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (file == -1)
 		return (-1);
 
