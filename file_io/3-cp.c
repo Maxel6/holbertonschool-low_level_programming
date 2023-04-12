65daac76e745afc24da70adf51e5a2779369de51
Axel Malka <axelmalka@outlook.fr>
2023-04-12 03:22:13 -0700

    corrected betty and add documentation

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,35 +1,40 @@
 #include "main.h"
-
+/**
+ * main - copie the content of a file to another file
+ * @av: arguments of the program
+ * @ac: number of arguments
+ * Return: always 0
+ */
 int main(int ac, char **av)
 {
-    int src, dest, size = 0;
-    char buffer[1024];
+	int src, dest, size = 0;
+	char buffer[1024];
 
-    if (ac != 3)      
-    {
-            dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		    exit(97);
-    }
-    src = open(av[1], O_RDONLY);
-    if (src == -1)
+	if (ac != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	src = open(av[1], O_RDONLY);
+	if (src == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 		exit(98);
 	}
-    
-    dest = open(av[2], O_CREAT | O_RDWR | O_TRUNC, 0664);
-    if (dest == -1)
-    {
-        dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
-        exit (99);
-    }
-  
-    while ((size = read(src, buffer, 1024)) > 0)
-    {
-        write(dest, buffer, size);
-    }
 
-    close(src);
-    close(dest);
-    return (0); 
+	dest = open(av[2], O_CREAT | O_RDWR | O_TRUNC, 0664);
+	if (dest == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+		exit(99);
+	}
+
+	while ((size = read(src, buffer, 1024)) > 0)
+	{
+		write(dest, buffer, size);
+	}
+
+	close(src);
+	close(dest);
+	return (0);
 }
