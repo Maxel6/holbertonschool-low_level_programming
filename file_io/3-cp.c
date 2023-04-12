0afb03602b8a1df7456e1bce35eb28ff62802175
= <=>
2023-04-12 11:06:20 +0200

    fix permissions

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -10,9 +10,7 @@ int main(int ac, char **av)
             dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		    exit(97);
     }
-
     src = open(av[1], O_RDONLY);
-    
     if (src == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
@@ -24,18 +22,14 @@ int main(int ac, char **av)
         dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
         exit(98);
     }
-
-    dest = open(av[2], O_CREAT | O_RDWR | O_TRUNC);
+    dest = open(av[2], O_CREAT | O_RDWR | O_TRUNC, 0664);
     if (dest == -1)
     {
         dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
         exit (99);
     }
-
     write(dest, buffer, strlen(buffer));
-
     close(src);
     close(dest);
-
-    return 0; 
+    return (0); 
 }
