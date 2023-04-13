02ba64f4dfa2f51b6da505ca470ff86d69f8da11
= <=>
2023-04-13 16:18:10 +0200

    fix existing node case

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -2,38 +2,31 @@
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-    unsigned long int index = key_index((const unsigned char *)key, ht->size);
-    hash_node_t * new;
-    char *cp_value = strdup(value);
-    int i = 0;
+	unsigned long int index = key_index((const unsigned char *)key, ht->size);
+	hash_node_t *new;
+	char *cp_value = strdup(value);
+	int i = 0;
 
-    new = malloc(sizeof(hash_node_t *) * ht->size);
-    if (ht->array[index] == NULL)
-            return (0);
-    new->value = cp_value;
-    new->key = (char *)key;
+	new = malloc(sizeof(hash_node_t *) * ht->size);
+	if (ht->array[index] == NULL)
+		return (0);
+	new->value = cp_value;
+	new->key = (char *)key;
 
-    if (!(ht->array[index]))
-    {
-        ht->array[index] = new;
-    }
-    else
-    {
-        while (ht->array[i])
-        {
-            if (strcmp(ht->array[i]->key, key) == 0)
-            {
-                ht->array[i]->value = cp_value;
-            }
-            else
-            {
-                new->next = ht->array[i];
-                ht->array[i] = new;
-            }
-            ht->array[i] = ht->array[i]->next;
-        }
-    }
+	while (ht->array[index]->next)
+	{
+		if (strcmp(ht->array[index]->key, key) == 0)
+		{
+			free(ht->array[index]->value);
+			ht->array[index]->value = cp_value;
+			return (1);
+		}
+		ht->array[index] = ht->array[index]->next;   
+	}
+	if (ht->array[index])
+		new->next = ht->array[index];
+	ht->array[index] = new;
 
-    free(cp_value);
-    return (1);
+	free(cp_value);
+	return (1);
 }
