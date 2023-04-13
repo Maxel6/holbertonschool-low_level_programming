666bf409317f381ed428f65ee6cf3dad1cad8b2c
= <=>
2023-04-13 13:52:26 +0200

    adding header and first task file

---
+++ hash_tables/0-hash_table_create.c
@@ -0,0 +1,12 @@
+#include "hash_tables.h"
+hash_table_t *hash_table_create(unsigned long int size)
+{
+    hash_table_t *ht = malloc (sizeof(hash_table_t));
+    
+    if (ht == NULL)
+        return NULL;
+    
+    ht->size = size;
+
+    return (ht);
+}
