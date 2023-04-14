538cdc7e568aa46e905febba88d538a279918158
= <=>
2023-04-14 15:52:06 +0200

    check if the table is empty

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -14,7 +14,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	index = key_index((const unsigned char *)key, ht->size);
 	new = malloc(sizeof(hash_node_t));
 
-	if (new == NULL || !ht)
+	if (new == NULL || ht == NULL)
 		return (0);
 	if (ht->array == NULL)
 	{
@@ -24,7 +24,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	new->value = strdup(value);
 	new->key = strdup(key);
-	if (new->key == NULL|| new->value == NULL)
+	if (new->key == NULL || new->value == NULL)
 		return (0);
 
 	if (ht->array[index])
