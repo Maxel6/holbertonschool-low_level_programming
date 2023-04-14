cef0ff06f0837c271e8043a4b142958158c8a45f
= <=>
2023-04-14 15:35:02 +0200

    fix Handled strdup return

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -24,6 +24,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	new->value = strdup(value);
 	new->key = strdup(key);
+	if (!new->key || !new->value)
+		return (0);
 
 	if (ht->array[index])
 	{
