cabfd2256bf1b4ff0c03902ce712d15083e629aa
Axel Malka <axelmalka@outlook.fr>
2023-05-23 11:09:26 +0000

    adding print if array is checked

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -8,6 +8,7 @@ int linear_search(int *array, size_t size, int value)
 	{
 		while (array[i])
 		{
+			printf("Value checked array[%d] = [%d]\n", i, array[i]);
 			if (array[i] == value)
 				return (i);
 			i++;
