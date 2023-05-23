4a6a2723992de13805afa1681197c1cc28a2f546
Axel Malka <axelmalka@outlook.fr>
2023-05-23 12:08:13 +0000

    betty corrected

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -1,5 +1,11 @@
 #include "search_algos.h"
-
+/**
+ * binary_search - search for a value using binary search
+ * @array: the array to search in
+ * @size: the size of the array
+ * @value: the value to check for
+ * Return: The index of the array contain the value, -1 else
+*/
 int binary_search(int *array, size_t size, int value)
 {
 	int l = 0, h = (int)size - 1, m = (h - l) / 2, i;
@@ -11,11 +17,10 @@ int binary_search(int *array, size_t size, int value)
 		{
 			printf(", %d", array[i]);
 		}
-			
 		printf("\n");
 		m = l + (h - l) / 2;
 		if (array[m] == value)
-			return m;
+			return (m);
 
 		if (array[m] < value)
 			l = m + 1;
@@ -24,5 +29,5 @@ int binary_search(int *array, size_t size, int value)
 			h = m - 1;
 	}
 
-	return -1;
+	return (-1);
 }
